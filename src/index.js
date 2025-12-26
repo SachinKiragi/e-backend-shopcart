bb506940c95d5f2fbfef6993aa9779d573e39c57
SachinKiragi <sachinkiragi8@gmail.com>
2025-12-26 13:23:29 +0530

    added basic middleware

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const responseTime = require('response-time');
 const app = express();
 const { PORT } = require('./config/server_config');
 const apiRouter = require('./routes/api_router');
 
+app.use(responseTime(function(req, res, time){
+    console.log("time: ", time);
+}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/api', apiRouter);
 
--- src/routes/v1/ping_router.js
+++ src/routes/v1/ping_router.js
@@ -2,6 +2,24 @@ const express = require('express');
 const { pingController } = require('../../controllers/v1/ping_controller');
 const pingRouter = express.Router();
 
-pingRouter.get('/', pingController);
+function chekRequest(request, response, next){
+    console.log("Checking request");
+    next();
+    console.log("ending Checking request");
+}
+
+function logger(request, response, next){
+    console.log("In logger");
+    next();
+    console.log("ending In logger");
+}
+
+function authChecker(request, response, next){
+    console.log("In auth Chekcer");
+    next();
+    console.log("ending In auth Chekcer");
+}
+
+pingRouter.get('/', [chekRequest, logger, authChecker], pingController);
 
 module.exports = pingRouter;
